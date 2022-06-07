7a9f62977ae2c1709e9ac62cc0b6d9de67cc3e20
Omarhuba <omaruba@gmail.com>
2022-06-07 23:29:51 +0200

    responsive version is done, pls checkout

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Quoto } from './components/Quoto'
 import { Weather } from './components/Weather'
 import { Showcase } from './components/Showcase'
 import { Footer } from './components/Footer'
+import img from './assets/images/logo.png'
 
 function App() {
   return (
     <div className="App">
       <header>
         <div className='headerView'>
+          <img className='hidden-logo' src={img} alt="" width='100px' height='40px'/>
         </div>
         <Showcase  />
       </header>
