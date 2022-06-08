d3ce4a0b7e307545d9d55f964d8e3cc922f5841c
rashadul-haque-js <mohammed.haque@iths.se>
2022-06-08 15:23:53 +0200

    showcase components updated

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import '../scss/Showcase.scss'
+/* eslint-disable */
+import React, { useState, useEffect } from "react";
+import "../scss/Showcase.scss";
+import programs  from "./program.json";
 
 export const Showcase = () => {
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setIndex((index) => {
+        if (index < programs.length - 1) {
+          return index + 1;
+        } else {
+          return 0;
+        }
+      });
+    }, 3000);
+
+    return () => clearInterval(intervalID);
+  }, []);
+
+
+
   return (
-    <div className='showcase'>
-        <h1>this is Showcase</h1>
+    <div className="showcase">
+      <img src={programs[index].image} />
+      <h1>{programs[index].program_name}</h1>
+      <h1>{programs[index].Last_date}</h1>
     </div>
-  )
-}
+  );
+};
