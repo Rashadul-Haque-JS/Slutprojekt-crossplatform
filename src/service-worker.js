6ad434de7a63dcfc2f52a5dd2f0c45f4f3e48887
rashadul-haque-js <mohammed.haque@iths.se>
2022-06-10 14:25:04 +0200

    SW offline

--- src/service-worker.js
+++ src/service-worker.js
@@ -14,33 +14,19 @@ self.addEventListener("install", (event) => {
 addEventListener("fetch", (event) => {
   if (!(event.request.url.indexOf("http") === 0)) return;
   event.respondWith(
-    // look in the cache for the resource
     caches.match(event.request).then(async (response) => {
       if (response) {
-        // is in cache, respond with the cached resource
         return response;
       }
-      // if not found fetch it from the network
+
       const networkResponse = await fetch(event.request);
-      // response needs to be cloned if going to be used more than once
+
       const clonedResponse = networkResponse.clone();
 
-      // save response to runtime cache for later use
       const runtimeCache = await caches.open("RAOM");
       runtimeCache.put(event.request, networkResponse);
 
-      // respond with the cloned network response
       return Promise.resolve(clonedResponse);
     })
   );
 });
-
-// self.addEventListener("install", event => {
-//   console.log("Installing!")
-//   self.skipWaiting()
-// })
-
-// self.addEventListener("activate", event => {
-//   console.log("Activate!")
-
-// })
