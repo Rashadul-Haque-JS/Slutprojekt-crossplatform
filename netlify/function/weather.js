90a45959636beb9a55fbe0eb66199c2ae940872d
rashadul-haque-js <mohammed.haque@iths.se>
2022-06-14 19:28:51 +0200

    added netlify functions

--- netlify/function/weather.js
+++ netlify/function/weather.js
@@ -1,14 +1,12 @@
 
 const axios = require('axios')
 
-const weatherHandler = async (event, context) => {
+const handler = async (event, context) => {
     const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.WEATHER_API_KEY}&&units=metric`)
     return {
         statusCode: 200,
-        body: JSON.stringify({
-        api: response.data
-        }),
+        body: JSON.stringify(response.data)
     }
 }
 
-module.exports ={weatherHandler}
+module.exports ={handler}
